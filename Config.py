c9a3f25389ab5951b3be5310b5913b84495cc9a7
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-29 10:17:45 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,13 +4,10 @@
 import os
 from os import environ
 
-class Config(object):
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-    API_ID = int(os.environ.get("API_ID", 12345))
-
-    API_HASH = os.environ.get("API_HASH")
 
 #CONSTANT VARIABLES
 base_url='https://ifsc.razorpay.com/' 
