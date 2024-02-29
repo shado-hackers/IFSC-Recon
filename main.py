23f1eb290dd9f5a7dc3874231409117279539803
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-29 09:56:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from pyrogram import *
 import requests as req
 from Config import *
+from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
+import wget
+import os 
 
-
-app=Client('IFSC-Recon',
+app=Client('xBot',
            api_id=API_ID,
            api_hash=API_HASH,
            bot_token=BOT_TOKEN)
